1e2b74f9b592fb1bfeae31d7382b9e3615e83f22
Italo Paiva <italosensei@hotmail.com>
2017-07-06 17:07:19 -0300

    Testing activity grades update

--- src/codeschool/sparta/tests/test_activity.py
+++ src/codeschool/sparta/tests/test_activity.py
@@ -1,10 +1,11 @@
-import pytest
-
-from mock import patch, Mock, MagicMock, mock
+from codeschool.accounts.factories import UserFactory
 from codeschool.models import User
-from types import SimpleNamespace
 from codeschool.sparta.models import SpartaActivity, UserGrade
 from codeschool.sparta.models.activity import read_csv_file
+
+import pytest
+from mock import patch, Mock, MagicMock, mock
+from types import SimpleNamespace
 
 
 class TestActivity:
@@ -63,3 +64,47 @@ class TestActivity:
                 csv = activity.create_post_grade_csv()
 
                 assert csv == csv_data_should_be
+
+
+class TestUserGrade:
+    @pytest.fixture
+    def activity_with_grades(self):
+        # Creating students and their grades in database to be able to change the grade
+        grades = [10,100,50,80]
+        teacher, *students = UserFactory.create_batch(5)
+        activity = SpartaActivity.objects.create(teacher=teacher, groups_size=2, title='Sparta')
+        for student, grade in zip(students, grades):
+            UserGrade.objects.create(grade=grade, activity=activity, user=student)
+        return (activity, students)
+
+    @pytest.mark.django_db
+    def test_grade_update_of_a_single_user(self, activity_with_grades):
+        activity, students = activity_with_grades
+        first_student = students[0]
+        new_grade = 75
+        mapping = {str(first_student.pk): new_grade}
+
+        UserGrade.update_activity_grades(activity, mapping)
+
+        first_student_grade = activity.user_grades.get(user=first_student)
+        assert first_student_grade.grade == new_grade
+
+    @pytest.mark.django_db
+    def test_grades_update_of_two_users(self, activity_with_grades):
+        activity, students = activity_with_grades
+        second_student = students[1]
+        third_student = students[2]
+        second_new_grade = 0
+        third_new_grade = 35.5
+        mapping = {
+            str(second_student.pk): second_new_grade,
+            str(third_student.pk): third_new_grade,
+        }
+
+        UserGrade.update_activity_grades(activity, mapping)
+
+        second_student_grade = activity.user_grades.get(user=second_student)
+        assert second_student_grade.grade == second_new_grade
+
+        third_student_grade = activity.user_grades.get(user=third_student)
+        assert third_student_grade.grade == third_new_grade
